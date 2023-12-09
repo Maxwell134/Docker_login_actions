cf4be6a461ec79626935358b6b5c843417e8740e
Maxwell134 <69123967+Maxwell134@users.noreply.github.com>
2023-12-09 18:08:24 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 # Accessing the GitHub actor context variable
 echo "Logging into Docker Hub with username: $INPUT_USERNAME as GitHub user: $GITHUB_ACTOR"
 apt-get update -y && apt-get install docker.io -y
-docker login -u $INPUT_USERNAME --password-stdin
+docker login -u $INPUT_USERNAME -p $INPUT_PASSWORD
