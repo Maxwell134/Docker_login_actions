ed1db7c7a6b01d28477b78ee5f33e5ab29d4ece5
Maxwell134 <69123967+Maxwell134@users.noreply.github.com>
2023-12-11 02:15:32 +0530

    Update login.py

--- login.py
+++ login.py
@@ -16,15 +16,19 @@ def build_and_push_image(image_name, image_tag, username):
     subprocess.run(build_tag, shell=True, check=True)
     subprocess.run(push_cmd, shell=True, check=True)
 
-def image_exists(username, image_name, image_tag):
+def image_exists(username, image_name, image_tag, token):
     api_url = f'https://hub.docker.com/v2/repositories/{username}/{image_name}/tags/{image_tag}/'
-    response = requests.head(api_url)
+    headers = {'Authorization': f'Bearer {token}'}
+
+    response = requests.head(api_url, headers=headers)
     return response.status_code == 200
 
-def delete_image(username, image_name, image_tag, password):
+
+def delete_image(username, image_name, image_tag, token):
     api_url = f'https://hub.docker.com/v2/repositories/{username}/{image_name}/tags/{image_tag}/'
-    auth = requests.auth.HTTPBasicAuth(username, password)
-    response = requests.delete(api_url, auth=auth)
+    headers = {'Authorization': f'Bearer {token}'}
+
+    response = requests.delete(api_url, headers=headers)
     return response.status_code == 204
 
 def main():
@@ -33,16 +37,22 @@ def main():
     image_tag = os.environ.get('image_tag')
     username = os.environ.get('username')
     password = os.environ.get('password')
+    token_id = os.environ.get('token')
 
     # Perform Docker login
     docker_login(username, password)
 
     # Check if image with tag exists in Docker Hub
-    if image_exists(username, image_name, image_tag):
-        print(f"Image with tag {image_tag} exists. Deleting from Docker Hub...")
-        # Delete the existing image from Docker Hub
-        delete_image(username, image_name, image_tag, password)
+    if not (username and token and image_name and image_tag):
+        print("Please provide Docker Hub credentials, image name, and image tag.")
+        return
 
+    # Check if the image with the specified tag exists in Docker Hub
+    if image_exists(username, image_name, image_tag, token):
+        print(
+            f"Image with tag {image_tag} exists. Deleting from Docker Hub...")
+        # Delete the existing image from Docker Hub
+        delete_image(username, image_name, image_tag, token)
     # Build and push Docker image
     build_and_push_image(image_name, image_tag, username)
 
