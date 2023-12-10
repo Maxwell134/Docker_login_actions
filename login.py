e559dfa114787bfdb34c3d9f1c6a3e077d2b488c
Maxwell134 <69123967+Maxwell134@users.noreply.github.com>
2023-12-10 22:49:36 +0530

    Update login.py

--- login.py
+++ login.py
@@ -8,16 +8,21 @@ def docker_login(username, password):
 
 def build_and_push_image(image_name, image_tag):
     build_cmd = f'docker build -t {image_name}:{image_tag} .'
-    # push_cmd = f'docker push {username}/{image_name}:{image_tag}'
+    build_tag = f'docker tag { image_name }:{ image_tag } { username }/{ image_name }:{ image_tag }
+
+    push_cmd = f'docker push {username}/{image_name}:{image_tag}'
 
     subprocess.run(build_cmd, shell=True, check=True)
-    # subprocess.run(push_cmd, shell=True, check=True)
+    subprocess.run(build_tag, shell=True, check=True)
+    subprocess.run(push_cmd, shell=True, check=True)
+    
+    
 
 def main():
     # Get environment variables from GitHub Actions inputs
     image_name = os.environ.get('image')
     image_tag = os.environ.get('image_tag')
-    # registry_url = os.environ.get('registry_url')
+    registry_url = os.environ.get('registry_url')
     username = os.environ.get('username')
     password = os.environ.get('password')
 
@@ -31,11 +36,13 @@ def main():
     details = {
         "image_name": image_name,
         "image_tag": image_tag,
-        # "registry_url": registry_url,
+        "registry_url": registry_url,
         
     }
 
     print(json.dumps(details, indent=2))
+    api_url = f'https://hub.docker.com/v2/repositories/{username}/{image_name}/{image_tag}'
+    print(api_url)
 
 if __name__ == "__main__":
     main()
