6d72eef397760f47f628a96dfa086673ecf5eb39
Maxwell134 <69123967+Maxwell134@users.noreply.github.com>
2023-12-13 00:55:51 +0530

    Update entrypoint.sh

--- .github/actions/login/entrypoint.sh
+++ .github/actions/login/entrypoint.sh
@@ -2,6 +2,7 @@
 set -e
 
 echo "Logging in to Docker registry..."
+apt-get install docker.io -y
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin "$DOCKER_REGISTRY"
 
 echo "Docker login successful!"
