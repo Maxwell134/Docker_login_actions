cdc2c1403ae50c49fad8c443ac8bda7e00ef8cac
Maxwell134 <69123967+Maxwell134@users.noreply.github.com>
2023-12-12 18:01:02 +0530

    Update install-python.sh

--- install-python.sh
+++ install-python.sh
@@ -1,9 +1,22 @@
-# install-python.sh
 #!/bin/bash
 
 set -e
 
 echo "Installing Python $1"
+
 # Add your actual commands to install Python based on the provided version
-apt-get update -y && apt-get install python3 -y
-chmod +x install-python
+if [ "$1" == "3.8" ]; then
+    # Example: Install Python 3.8 using apt
+    sudo apt-get update
+    sudo apt-get install -y python3.8
+elif [ "$1" == "3.9" ]; then
+    # Example: Install Python 3.9 using apt
+    sudo apt-get update
+    sudo apt-get install -y python3.9
+else
+    echo "Unsupported Python version: $1"
+    exit 1
+fi
+
+# Show Python version
+python3 --version
